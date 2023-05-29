7ff7551ee9b707c4d2af980693add676fd7fd2d1
Gabriel Sá <t6w311@gmail.com>
2023-05-28 23:23:44 -0300

    change(states): change location and name

---
+++ src/components/fishingSimulator/mainScreen/mainScreenStates.js
@@ -0,0 +1,8 @@
+const mainScreenStates = {
+    Idle: 'idle',
+    Casting: 'casting',
+    WaitingToHook: 'waitingToHook',
+    Fishing: 'fishing',
+}
+
+export default mainScreenStates;
--- src/components/fishingSimulator/states/index.js
+++
@@ -1,7 +0,0 @@
-const states = {
-    Idle: 'idle',
-    Casting: 'casting',
-    Fishing: 'fishing',
-}
-
-export default states;
